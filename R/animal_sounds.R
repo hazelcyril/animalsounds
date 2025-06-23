07f89a38dc2be4b75415a1eed1420e3c5781bf49
hazelcyril <hazel.cyril@warwick.ac.uk>
2025-06-23 13:42:16 +0100

    Updated Documentation with markdown

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,4 +1,8 @@
-#' Animal Sounds
+#' Print the Sounds that Animals Make
+#'
+#'Print the sound that an animal makes in the console
+#'
+#' Is a wrapper to [paste0()]. This is a *silly function* in a **toy package**.
 #'
 #' @param animal name of animal, takes in string value
 #' @param sound sound made by the animal, takes in string value
@@ -8,11 +12,11 @@
 #'
 #' @examples
 #' animal_sounds ("pig", "oink")
+##  Try `animal_sounds ("pig", "oink")`- doesnt work
+
 animal_sounds <- function(animal, sound)
 {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
   paste0("The ", animal, " goes ", sound, "!")
 }
-
-?animal_sounds
