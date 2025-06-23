6081c2ec780b097f4958d0b267fdbd2d30a05dd7
hazelcyril <hazel.cyril@warwick.ac.uk>
2025-06-23 13:25:00 +0100

    Added documentation for animal_sounds Exported animal_sounds

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,6 +1,18 @@
+#' Animal Sounds
+#'
+#' @param animal name of animal, takes in string value
+#' @param sound sound made by the animal, takes in string value
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' animal_sounds ("pig", "oink")
 animal_sounds <- function(animal, sound)
 {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
   paste0("The ", animal, " goes ", sound, "!")
 }
+
+?animal_sounds
